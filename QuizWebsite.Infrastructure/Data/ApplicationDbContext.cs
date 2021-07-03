401dfbd14c4bdcadfa68e1282a23724cd3a83fa8
NajiseyedhosseinzadehShahin <Shahin.Najiseyedhosseinzadeh@student.howest.be>
2021-07-03 16:48:03 +0200

    Add dbContext

---
+++ QuizWebsite.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuizWebsite.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuizWebsite.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Question> Questions { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :
+        base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
