8c34452cf696bdeed8c9257fd565c5fd4c0df106
NajiseyedhosseinzadehShahin <Shahin.Najiseyedhosseinzadeh@student.howest.be>
2021-07-04 21:13:40 +0200

    Add Question interface

---
+++ QuizWebsite.Core/Interfaces/IQuestionService.cs
@@ -0,0 +1,14 @@
+﻿using QuizWebsite.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuizWebsite.Core.Interfaces
+{
+    public interface IQuestionService
+    {
+        ICollection<Question> GetQuestions(); //RandomizedOrder
+        Question GetQuestionById(Guid id);
+        Question GetRandomQuestion();
+    }
+}
---
+++ QuizWebsite.Web/Models/SingePlayerUnlimitedVm.cs
@@ -0,0 +1,14 @@
+﻿using QuizWebsite.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizWebsite.Web.Models
+{
+    public class SingePlayerUnlimitedVm
+    {
+        public Player Player { get; set; }
+        public List<Question> Questions { get; set; }
+    }
+}
