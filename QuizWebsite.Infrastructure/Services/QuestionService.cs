d54eb3e857e4048b48d3cc2a622955fc27e33690
NajiseyedhosseinzadehShahin <Shahin.Najiseyedhosseinzadeh@student.howest.be>
2021-07-04 21:32:48 +0200

    Add QuestionService

---
+++ QuizWebsite.Infrastructure/Services/QuestionService.cs
@@ -0,0 +1,26 @@
+﻿using QuizWebsite.Core.Entities;
+using QuizWebsite.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuizWebsite.Infrastructure.Services
+{
+    public class QuestionService : IQuestionService
+    {
+        public Question GetQuestionById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Question> GetQuestionsRandomOrder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Question GetRandomQuestion()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
