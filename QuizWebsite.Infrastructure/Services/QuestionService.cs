aff4d3d2c8a5b882a689e349b324912cd619b6cf
NajiseyedhosseinzadehShahin <Shahin.Najiseyedhosseinzadeh@student.howest.be>
2021-07-04 22:52:06 +0200

    Implement services

--- QuizWebsite.Infrastructure/Services/QuestionService.cs
+++ QuizWebsite.Infrastructure/Services/QuestionService.cs
@@ -1,26 +1,61 @@
-﻿using QuizWebsite.Core.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using QuizWebsite.Core.Entities;
 using QuizWebsite.Core.Interfaces;
+using QuizWebsite.Infrastructure.Data;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace QuizWebsite.Infrastructure.Services
 {
     public class QuestionService : IQuestionService
     {
+        protected readonly ApplicationDbContext dbContext;
+        public QuestionService(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
         public Question GetQuestionById(Guid id)
         {
-            throw new NotImplementedException();
+            return GetQuestions().SingleOrDefault(q => q.Id == id);
+        }
+
+        public ICollection<Question> GetQuestions()
+        {
+            return dbContext.Questions.ToList();
         }
 
         public ICollection<Question> GetQuestionsRandomOrder()
         {
-            throw new NotImplementedException();
+            var questions = GetQuestions().ToList();
+            return Shuffle(questions);
         }
 
         public Question GetRandomQuestion()
         {
-            throw new NotImplementedException();
+            var questions = GetQuestions().ToList();
+            Random rnd = new Random();
+            return questions[rnd.Next(questions.Count)];
         }
+
+        private List<T> Shuffle<T>(List<T> list)
+        {
+            Random rng = new Random();
+
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+
+            return list;
+        }
+
+
     }
 }
