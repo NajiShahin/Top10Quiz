6c4659bd31e16ce4b3a2060ee2bbdcf3e0c02733
NajiseyedhosseinzadehShahin <Shahin.Najiseyedhosseinzadeh@student.howest.be>
2021-07-05 22:02:17 +0200

    Add IQuestionService

--- QuizWebsite.Core/Interfaces/Services/IQuestionService.cs
+++ QuizWebsite.Core/Interfaces/Services/IQuestionService.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using QuizWebsite.Core.Dtos;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace QuizWebsite.Core.Interfaces.Services
 {
     public interface IQuestionService
     {
+        Task<IEnumerable<QuestionResponseDto>> ListAllAsync();
+        Task<QuestionResponseDto> GetByIdAsync(Guid id);
+        Task<QuestionResponseDto> AddAsync(QuestionRequestDto questionRequest);
+        Task<QuestionResponseDto> UpdateAsync(QuestionRequestDto questionRequest);
+        Task DeleteAsync(Guid id);
     }
 }
