cc06c664ffad59edb29075d1e35b8f58fc439cf2
NajiseyedhosseinzadehShahin <Shahin.Najiseyedhosseinzadeh@student.howest.be>
2021-07-05 19:38:35 +0200

    Add mapping

---
+++ QuizWebsite.Core/Mapping/AutoMapperProfiles.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using QuizWebsite.Core.Dtos;
+using QuizWebsite.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuizWebsite.Core.Mapping
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<QuestionRequestDto, Question>();
+            CreateMap<Question, QuestionResponseDto>();
+
+            CreateMap<AnswerRequestDto, Answer>();
+            CreateMap<Answer, AnswerResponseDto>();
+
+            CreateMap<CategoryRequestDto, Category>();
+            CreateMap<Category, CategoryResponseDto>();
+
+        }
+    }
+}
