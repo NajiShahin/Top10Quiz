0866b0821a41da4c9a9323c307d614252efdc60d
NajiseyedhosseinzadehShahin <Shahin.Najiseyedhosseinzadeh@student.howest.be>
2021-07-05 23:45:14 +0200

    Add questionController

---
+++ QuizWebsite.Api/Controllers/QuestionController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuizWebsite.Core.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizWebsite.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionController : ControllerBase
+    {
+        private readonly IQuestionService questionService;
+        public QuestionController(IQuestionService questionService)
+        {
+            this.questionService = questionService;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var questions = await questionService.ListAllAsync();
+            return Ok(questions);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            var question = await questionService.GetByIdAsync(id);
+            if (question == null)
+            {
+                return NotFound($"Event with ID {id} does not exist");
+            }
+            return Ok(question);
+        }
+    }
+}
