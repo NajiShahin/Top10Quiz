8bb1b83fc6f324290a64b7e10a85ee5939c34fa6
NajiseyedhosseinzadehShahin <Shahin.Najiseyedhosseinzadeh@student.howest.be>
2021-07-05 23:18:48 +0200

    Add questionRepository

---
+++ QuizWebsite.Infrastructure/Repositories/QuestionRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuizWebsite.Core.Entities;
+using QuizWebsite.Core.Interfaces.Repositories;
+using QuizWebsite.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizWebsite.Infrastructure.Repositories
+{
+    public class QuestionRepository : EfRepository<Question>, IQuestionRepository
+    {
+        public QuestionRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public override async Task<Question> GetByIdAsync(Guid id)
+        {
+            return await GetAllAsync().SingleOrDefaultAsync(a => a.Id.Equals(id));
+        }
+        public override IQueryable<Question> GetAllAsync()
+        {
+            return _dbContext.Questions.AsNoTracking();
+        }
+    }
+}
