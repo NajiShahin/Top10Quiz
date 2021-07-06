8d3238bd5d4b2b3f7723e806f110dd946cedf66d
NajiseyedhosseinzadehShahin <Shahin.Najiseyedhosseinzadeh@student.howest.be>
2021-07-06 14:46:17 +0200

    Add extra endpoints for Questions

--- QuizWebsite.Api/Controllers/QuestionController.cs
+++ QuizWebsite.Api/Controllers/QuestionController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using QuizWebsite.Core.Dtos;
 using QuizWebsite.Core.Interfaces.Services;
 using System;
 using System.Collections.Generic;
@@ -34,6 +35,32 @@ namespace QuizWebsite.Api.Controllers
                 return NotFound($"Question with ID {id} does not exist");
             }
             return Ok(question);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(QuestionRequestDto questionRequest)
+        {
+            var questionResponseDto = await questionService.AddAsync(questionRequest);
+            return CreatedAtAction(nameof(Get), new { id = questionResponseDto.Id }, questionResponseDto);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put(QuestionRequestDto questionRequest)
+        {
+            var questionResponseDto = await questionService.UpdateAsync(questionRequest);
+            return Ok(questionResponseDto);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var questionDto = await questionService.GetByIdAsync(id);
+            if (questionDto == null)
+            {
+                return NotFound($"Question with ID {id} does not exist");
+            }
+            await questionService.DeleteAsync(id);
+            return Ok();
         }
     }
 }
