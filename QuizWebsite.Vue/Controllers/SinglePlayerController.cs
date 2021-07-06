d5f8abee6b6b39f5798d477818fd1a4c19ed3f45
NajiseyedhosseinzadehShahin <Shahin.Najiseyedhosseinzadeh@student.howest.be>
2021-07-06 16:05:42 +0200

    Add singlePlayerController

---
+++ QuizWebsite.Vue/Controllers/SinglePlayerController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizWebsite.Vue.Controllers
+{
+    public class SinglePlayerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Classic()
+        {
+            return View();
+        }
+
+        public IActionResult Unlimited()
+        {
+            return View();
+        }
+    }
+}
