a870bdc9e8cc5fa2e9ea5780774004ac6b92208d
NajiseyedhosseinzadehShahin <Shahin.Najiseyedhosseinzadeh@student.howest.be>
2021-07-12 00:39:07 +0200

    Add place to answer

--- QuizWebsite.Core/Entities/Answer.cs
+++ QuizWebsite.Core/Entities/Answer.cs
@@ -8,6 +8,7 @@ namespace QuizWebsite.Core.Entities
     public class Answer : EntityBase
     {
         public int Points { get; set; } //How many points you get from the answer
+        public int Place { get; set; } //Same as points, but if 2 different answers have same amount of points you this will be different.
         public string AnswerText { get; set; }
         public string ExtraInfo { get; set; } //If the question is biggest population countries this will be the population of the country Ex. AnswerText = China, ExtraInfo = 1,444,948,110
         public Guid QuestionId { get; set; }
