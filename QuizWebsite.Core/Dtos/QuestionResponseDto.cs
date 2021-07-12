bc45ede4cef521f57f46737fad8d910fd2fdcc66
NajiseyedhosseinzadehShahin <Shahin.Najiseyedhosseinzadeh@student.howest.be>
2021-07-12 14:45:48 +0200

    Change name property

--- QuizWebsite.Core/Dtos/QuestionResponseDto.cs
+++ QuizWebsite.Core/Dtos/QuestionResponseDto.cs
@@ -8,6 +8,6 @@ namespace QuizWebsite.Core.Dtos
     {
         public string QuestionText { get; set; }
         public ICollection<CategoryResponseDto> Category { get; set; }
-        public ICollection<AnswerDetailRequestDto> MyProperty { get; set; }
+        public ICollection<AnswerDetailRequestDto> Answers { get; set; }
     }
 }
