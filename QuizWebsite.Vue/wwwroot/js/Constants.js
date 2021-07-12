5e10ffa6852da98dca1909ea5003e50064855a76
NajiseyedhosseinzadehShahin <Shahin.Najiseyedhosseinzadeh@student.howest.be>
2021-07-13 00:53:01 +0200

    Add constants

--- QuizWebsite.Vue/wwwroot/js/Constants.js
+++ QuizWebsite.Vue/wwwroot/js/Constants.js
@@ -1,1 +1,3 @@
-﻿const RandomQuestionListUrl = "https://localhost:5001/api/question/randomize"
+﻿const RandomQuestionListUrl = "https://localhost:5001/api/question/randomize";
+const QuestionUrl = "https://localhost:5001/api/question/";
+const url = "https://localhost:5001/";
