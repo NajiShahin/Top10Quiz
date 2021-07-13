4e71c002ee1b77fba35f3d1b6f0e6bfd0cbbf07d
NajiseyedhosseinzadehShahin <Shahin.Najiseyedhosseinzadeh@student.howest.be>
2021-07-13 21:07:13 +0200

    Add question type

--- QuizWebsite.Core/Entities/Question.cs
+++ QuizWebsite.Core/Entities/Question.cs
@@ -8,6 +8,7 @@ namespace QuizWebsite.Core.Entities
     public class Question : EntityBase
     {
         public string QuestionText { get; set; }
+        public string QuestionType { get; set; } //Top10 = top10 quiz, other types can also be created
         public Guid CategoryId { get; set; }
         public ICollection<CategoryQuestions> CategoryQuestions { get; set; }
         public ICollection<Answer> Answers { get; set; }
