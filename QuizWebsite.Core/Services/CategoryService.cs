68bb585fcfffe30f04c8bc5c2d7af7da0c06068a
NajiseyedhosseinzadehShahin <Shahin.Najiseyedhosseinzadeh@student.howest.be>
2021-07-15 18:03:37 +0200

    Add categoryService

---
+++ QuizWebsite.Core/Services/CategoryService.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using QuizWebsite.Core.Dtos;
+using QuizWebsite.Core.Interfaces.Repositories;
+using QuizWebsite.Core.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizWebsite.Core.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepository categoryRepository;
+        private readonly IMapper mapper;
+        public CategoryService(ICategoryRepository categoryRepository,
+            IMapper mapper)
+        {
+            this.categoryRepository = categoryRepository;
+            this.mapper = mapper;
+        }
+        public async Task<IEnumerable<CategoryResponseDto>> ListAllAsync()
+        {
+            var result = await categoryRepository.ListAllAsync();
+            var dto = mapper.Map<IEnumerable<CategoryResponseDto>>(result);
+            return dto;
+        }
+    }
+}
