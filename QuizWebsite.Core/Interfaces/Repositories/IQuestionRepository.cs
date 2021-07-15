db1471f6e741fb9d97078b3f8675efd554bfb831
NajiseyedhosseinzadehShahin <Shahin.Najiseyedhosseinzadeh@student.howest.be>
2021-07-16 00:49:51 +0200

    Add method to interface

--- QuizWebsite.Core/Interfaces/Repositories/IQuestionRepository.cs
+++ QuizWebsite.Core/Interfaces/Repositories/IQuestionRepository.cs
@@ -2,11 +2,12 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace QuizWebsite.Core.Interfaces.Repositories
 {
     public interface IQuestionRepository : IRepository<Question>
     {
-
+        Task<IEnumerable<Question>> SearchByCategoriesAsync(string categoryIds);
     }
 }
