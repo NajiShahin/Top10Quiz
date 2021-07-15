4af34dc09e9473488fcbe10f9d297b7c51300c9a
NajiseyedhosseinzadehShahin <Shahin.Najiseyedhosseinzadeh@student.howest.be>
2021-07-15 17:55:14 +0200

    Add interfaces for categories

---
+++ QuizWebsite.Core/Interfaces/Repositories/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using QuizWebsite.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuizWebsite.Core.Interfaces.Repositories
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+
+    }
+}
---
+++ QuizWebsite.Core/Interfaces/Services/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using QuizWebsite.Core.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizWebsite.Core.Interfaces.Services
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<CategoryResponseDto>> ListAllAsync();
+    }
+}
