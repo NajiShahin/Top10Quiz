93a6d0fa8075803b95f0b228de36c630c4b717c0
NajiseyedhosseinzadehShahin <Shahin.Najiseyedhosseinzadeh@student.howest.be>
2021-07-17 00:12:29 +0200

    Add questions

--- QuizWebsite.Infrastructure/Data/Seeding/CategorySeeder.cs
+++ QuizWebsite.Infrastructure/Data/Seeding/CategorySeeder.cs
@@ -11,7 +11,9 @@ namespace QuizWebsite.Infrastructure.Data.Seeding
         public static void Seed(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Category>().HasData(
-new Category { Id = Guid.Parse("00000000-0000-0000-0000-100000000001"), Name = "Countries" }
+new Category { Id = Guid.Parse("00000000-0000-0000-0000-100000000001"), Name = "Countries" },
+new Category { Id = Guid.Parse("00000000-0000-0000-f000-900000000007"), Name = "History" },
+new Category { Id = Guid.Parse("00000000-1001-1001-1001-500000100005"), Name = "People" }
 
                 );
         }
