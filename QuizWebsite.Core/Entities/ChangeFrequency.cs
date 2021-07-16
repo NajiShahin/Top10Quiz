a5383b4f4276242190687d26b4d82de8e9bc8f03
NajiseyedhosseinzadehShahin <Shahin.Najiseyedhosseinzadeh@student.howest.be>
2021-07-16 23:53:04 +0200

    Add new properties and enum

---
+++ QuizWebsite.Core/Entities/ChangeFrequency.cs
@@ -0,0 +1,14 @@
+﻿namespace QuizWebsite.Core.Entities
+{
+    public enum ChangeFrequency
+    {
+        Weekly = 0,
+        Monthly = 1,
+        ThreeMonthly = 2,
+        SixMonthly = 3,
+        Yearly = 4,
+        TwoYearly = 5,
+        FourYearly = 6,
+        Never = 7
+    }
+}
--- QuizWebsite.Core/Entities/Question.cs
+++ QuizWebsite.Core/Entities/Question.cs
@@ -9,6 +9,10 @@ namespace QuizWebsite.Core.Entities
     {
         public string QuestionText { get; set; }
         public string QuestionType { get; set; } //Top10 = top10 quiz, other types can also be created
+        public int Likes { get; set; }
+        public int Dislikes { get; set; }
+        public DateTime DateTimeChanged { get; set; }
+        public ChangeFrequency ChangeFrequency { get; set; } //Frequency that you'll manually have to check if answers are correct
         public ICollection<CategoryQuestions> CategoryQuestions { get; set; }
         public ICollection<Answer> Answers { get; set; }
     }
