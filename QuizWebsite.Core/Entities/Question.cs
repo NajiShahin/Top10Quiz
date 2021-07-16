3809780a3f0ed1ab5636f024deaa8b7fca697464
NajiseyedhosseinzadehShahin <Shahin.Najiseyedhosseinzadeh@student.howest.be>
2021-07-16 15:51:49 +0200

    Remove categoryId

--- QuizWebsite.Core/Entities/Question.cs
+++ QuizWebsite.Core/Entities/Question.cs
@@ -9,7 +9,6 @@ namespace QuizWebsite.Core.Entities
     {
         public string QuestionText { get; set; }
         public string QuestionType { get; set; } //Top10 = top10 quiz, other types can also be created
-        public Guid CategoryId { get; set; }
         public ICollection<CategoryQuestions> CategoryQuestions { get; set; }
         public ICollection<Answer> Answers { get; set; }
     }
