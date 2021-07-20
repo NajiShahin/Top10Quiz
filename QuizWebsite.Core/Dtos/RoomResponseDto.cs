bfdd516a471a098c2595ce4a33aaca03340e6132
NajiseyedhosseinzadehShahin <Shahin.Najiseyedhosseinzadeh@student.howest.be>
2021-07-20 19:03:19 +0200

    Add rooms

---
+++ QuizWebsite.Core/Dtos/RoomResponseDto.cs
@@ -0,0 +1,13 @@
+﻿using QuizWebsite.Core.Dtos.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuizWebsite.Core.Dtos
+{
+    public class RoomResponseDto : BaseDto
+    {
+        public bool Public { get; set; }
+        public int Players { get; set; }
+    }
+}
---
+++ QuizWebsite.Core/Entities/Room.cs
@@ -0,0 +1,13 @@
+﻿using QuizWebsite.Core.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuizWebsite.Core.Entities
+{
+    public class Room : EntityBase
+    {
+        public bool Public { get; set; }
+        public int Players { get; set; }
+    }
+}
