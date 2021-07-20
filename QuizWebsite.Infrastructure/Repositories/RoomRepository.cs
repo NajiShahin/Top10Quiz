ac25203e7916879a9b0318de8f3c3f1542844faa
NajiseyedhosseinzadehShahin <Shahin.Najiseyedhosseinzadeh@student.howest.be>
2021-07-21 00:02:58 +0200

    Add methods for rooms

---
+++ QuizWebsite.Infrastructure/Repositories/RoomRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuizWebsite.Core.Entities;
+using QuizWebsite.Core.Interfaces.Repositories;
+using QuizWebsite.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizWebsite.Infrastructure.Repositories
+{
+    public class RoomRepository : EfRepository<Room>, IRoomRepository
+    {
+
+        public RoomRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public override async Task<Room> GetByIdAsync(Guid id)
+        {
+            return await GetAllAsync().SingleOrDefaultAsync(a => a.Id.Equals(id));
+        }
+        public override IQueryable<Room> GetAllAsync()
+        {
+            return _dbContext.Rooms.AsNoTracking();
+        }
+    }
+}
