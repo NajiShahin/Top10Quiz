cbc65894963ec1847a046e49813806351d6fe368
NajiseyedhosseinzadehShahin <Shahin.Najiseyedhosseinzadeh@student.howest.be>
2021-07-22 20:57:22 +0200

    Add chars a room name can have

--- QuizWebsite.Infrastructure/Repositories/RoomRepository.cs
+++ QuizWebsite.Infrastructure/Repositories/RoomRepository.cs
@@ -68,7 +68,7 @@ namespace QuizWebsite.Infrastructure.Repositories
         private static string RandomString(int length)
         {
             Random random = new Random();
-            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            const string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
             return new string(Enumerable.Repeat(chars, length)
               .Select(s => s[random.Next(s.Length)]).ToArray());
         }
