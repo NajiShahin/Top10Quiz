faab5a5a8b9a6309074cc46ac29b030b96a91673
NajiseyedhosseinzadehShahin <Shahin.Najiseyedhosseinzadeh@student.howest.be>
2021-07-23 00:00:50 +0200

    Change porperties in player

--- QuizWebsite.Core/Entities/Player.cs
+++ QuizWebsite.Core/Entities/Player.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using QuizWebsite.Core.Entities.Base;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace QuizWebsite.Core.Entities
 {
-    public class Player
+    public class Player : EntityBase
     {
+        public string Name { get; set; }
+        public string ConnectionId { get; set; }
         public int Score { get; set; }
-        public int QuestionsAnwsered { get; set; }
+        public Guid RoomId { get; set; }
+        public Room Room { get; set; }
     }
 }
