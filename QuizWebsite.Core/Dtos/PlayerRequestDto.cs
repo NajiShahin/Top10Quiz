c921d8e38ffe84aad345b4ab1faadddb94c5ad5a
NajiseyedhosseinzadehShahin <Shahin.Najiseyedhosseinzadeh@student.howest.be>
2021-08-02 00:47:07 +0200

    Remove colorCode

--- QuizWebsite.Core/Dtos/PlayerRequestDto.cs
+++ QuizWebsite.Core/Dtos/PlayerRequestDto.cs
@@ -8,7 +8,6 @@ namespace QuizWebsite.Core.Dtos
     {
         public string Name { get; set; }
         public string ConnectionId { get; set; }
-        public string ColorCode { get; set; }
         public int Score { get; set; }
     }
 }
