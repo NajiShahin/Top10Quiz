c512d826aeaed06b683fe952c7bff71217c5070c
NajiseyedhosseinzadehShahin <Shahin.Najiseyedhosseinzadeh@student.howest.be>
2021-08-05 15:21:05 +0200

    Add properties to entities

--- QuizWebsite.Core/Entities/RoomQuestions.cs
+++ QuizWebsite.Core/Entities/RoomQuestions.cs
@@ -6,6 +6,7 @@ namespace QuizWebsite.Core.Entities
 {
     public class RoomQuestions
     {
+        int questionNumber;
         public Guid RoomId { get; set; }
         public Guid QuestionId { get; set; }
         public Room Room { get; set; }
