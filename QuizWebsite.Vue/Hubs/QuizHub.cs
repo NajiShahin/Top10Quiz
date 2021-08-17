a06607ce234de2e85ae5fd16adf7e40a0abe5ccc
NajiseyedhosseinzadehShahin <Shahin.Najiseyedhosseinzadeh@student.howest.be>
2021-08-17 20:02:59 +0200

    Add list of Users

--- QuizWebsite.Vue/Hubs/QuizHub.cs
+++ QuizWebsite.Vue/Hubs/QuizHub.cs
@@ -8,6 +8,8 @@ using System.Text.Json.Serialization;
 using QuizWebsite.Core.Dtos;
 using Newtonsoft.Json;
 using System.Text;
+using System.Collections.Concurrent;
+using System.Diagnostics;
 
 namespace QuizWebsite.Vue.Hubs
 {
@@ -16,11 +18,12 @@ namespace QuizWebsite.Vue.Hubs
         private readonly IHubContext<QuizHub> _hubContext;
         private Random rnd = new Random();
         private static readonly System.Timers.Timer _timer = new System.Timers.Timer();
+        public static ConcurrentDictionary<string, List<string>> ConnectedUsers = new ConcurrentDictionary<string, List<string>>();
 
         public QuizHub(IHubContext<QuizHub> hubContext)
-    {
+        {
             _hubContext = hubContext;
-            _timer.Interval = 10000;
+            _timer.Interval = 1000;
             _timer.Elapsed += TimerElapsed;
             _timer.Start();
         }
@@ -28,6 +31,10 @@ namespace QuizWebsite.Vue.Hubs
         void TimerElapsed(object sender, System.Timers.ElapsedEventArgs e)
         {
             int month = rnd.Next(1, 1000000);
+
+            /*var result = await httpClient.GetAsync("https://localhost:5001/api/Rooms" + this.Context.ConnectionId);
+            var data = await result.Content.ReadAsStringAsync();
+            var player = JsonConvert.DeserializeObject<PlayerResponseDto>(data);*/
 
             _hubContext.Clients.All.SendAsync("blob", month);
         }
@@ -68,6 +75,32 @@ namespace QuizWebsite.Vue.Hubs
             return this.Context.ConnectionId;
         }
 
+        public override Task OnConnectedAsync()
+        {
+            Trace.TraceInformation("MapHub started. ID: {0}", Context.ConnectionId);
+
+            var userName = "testUserName1"; // or get it from Context.User.Identity.Name;
+
+            // Try to get a List of existing user connections from the cache
+            List<string> existingUserConnectionIds;
+            ConnectedUsers.TryGetValue(userName, out existingUserConnectionIds);
+
+            // happens on the very first connection from the user
+            if (existingUserConnectionIds == null)
+            {
+                existingUserConnectionIds = new List<string>();
+            }
+
+            // First add to a List of existing user connections (i.e. multiple web browser tabs)
+            existingUserConnectionIds.Add(Context.ConnectionId);
+
+
+            // Add to the global dictionary of connected users
+            ConnectedUsers.TryAdd(userName, existingUserConnectionIds);
+
+            return Task.CompletedTask;
+        }
+
         public override async Task OnDisconnectedAsync(Exception ex)
         {
             HttpClient httpClient = new HttpClient();
@@ -80,6 +113,25 @@ namespace QuizWebsite.Vue.Hubs
                 await httpClient.DeleteAsync("https://localhost:5001/api/Rooms/" + room.Id);
             }
             await Clients.Group(room?.Name)?.SendAsync("userJoined");
+
+            //Remove from list
+            var userName = Context.User.Identity.Name;
+
+            List<string> existingUserConnectionIds;
+            ConnectedUsers.TryGetValue(userName, out existingUserConnectionIds);
+
+            // remove the connection id from the List 
+            existingUserConnectionIds.Remove(Context.ConnectionId);
+
+            // If there are no connection ids in the List, delete the user from the global cache (ConnectedUsers).
+            if (existingUserConnectionIds.Count == 0)
+            {
+                // if there are no connections for the user,
+                // just delete the userName key from the ConnectedUsers concurent dictionary
+                List<string> garbage; // to be collected by the Garbage Collector
+                ConnectedUsers.TryRemove(userName, out garbage);
+            }
+
         }
 
         public async Task Refresh(string groupName)
